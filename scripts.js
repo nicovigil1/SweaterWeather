a935a7b8b5cb9b2a257f5ba7c7751fde3e85dff4
Nico <nicovigil1@gmail.com>
2019-03-14 14:28:45 -0600

    adds ability for current weather to be displayed on the page on search (current default search: denver)

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 var searchBox = document.querySelector("#search-input")
+var currentWeather = document.querySelector(".current-weather")
 
 searchBox.addEventListener('keypress', function(key) {
   if (key.keyCode === 13) {
@@ -10,6 +11,9 @@ function getWeather() {
   fetch('https://blooming-island-81872.herokuapp.com/api/v1/forecast?location=denver,co')
     .then(response => {return response.json()})
     .then(data => {buildService(data)})
+    .then(showCurrentForecast)
+  moveSearchBox()
+  setTimeout(showCurrentWeather, 700)
 }
 
 function searchLocation() {
@@ -22,7 +26,37 @@ function buildService (raw_forecast) {
   }
 }
 
+
+
+
+// page renders
+function showCurrentForecast() {
+  let currentForecast = document.querySelector(".current-forecast")
+  let weather = service.current
+  currentForecast.innerHTML = `
+  <p>Current Temperature: ${weather.apparentTemperature}<p>
+  <p>Chance of Rain: ${weather.precipProbability} </p>
+  <p>Windspeed: ${weather.windSpeed} mph</p>
+  `
+}
+
+// animations 
 function hideSearchBox() {
   searchBox.style.display = 'none';
 }
 
+function showCurrentWeather() {
+  currentWeather.style.display = 'inline'
+}
+
+var pos = 45;
+function moveSearchBox() {
+  let searchDiv = document.querySelector('.search-box')
+  var interval = setInterval(moveSearchBox, 40)
+  if (pos === 60) {
+    clearInterval(interval)
+  } else {
+    pos++
+    searchDiv.style.bottom = pos + 'vh'
+  }
+}
